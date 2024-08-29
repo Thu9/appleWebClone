ac78122e975962f946263a9d64d8c74bf2c62f3c
Thu9 <leete416@naver.com>
2024-08-29 11:50:21 +0900

    Error page add

--- src/routes/pageRoutes.tsx
+++ src/routes/pageRoutes.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Main from "../pages/main";
 import Stroke from "../pages/stroke";
+import NotFound from "../pages/error";
 
 export const PageRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Main />}></Route>
       <Route path="stroke" element={<Stroke />}></Route>
+      <Route path="*" element={<NotFound />}></Route>
     </Routes>
   );
 };
